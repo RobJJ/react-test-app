52825f6f1541564a1c8799ed0c409f3346dccfd7
RobJ <robjjohnsenwork@gmail.com>
2022-02-25 14:53:04 +0700

    Change to Header

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is MasterBasil's react-test-app!
+          This is MasterBasil's react-test-app, second test!
         </p>
         
       </header>
